aa28d139fe820ca26f17c21e44a9ee41a7ad6304
Kechi <bonifacekechi@gmail.com>
2022-02-10 13:49:43 -0500

    Cors Fix

--- journal-api/settings.py
+++ journal-api/settings.py
@@ -154,6 +154,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:8000',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
